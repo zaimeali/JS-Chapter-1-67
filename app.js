480cd6faccf67832542c1ef9644b9942a3843e44
zaime ali <zaimeali1997@gmail.com>
2020-07-17 23:57:14 +0500

    On Chapter 38-42

---
+++ app.js
@@ -0,0 +1,59 @@
+// 
+// Chapters 38-42 (Functions, Switch Statement, While.. Do While Loops)
+// 
+
+        // Question 1
+    // function power(a, b){
+    //     let pow = 1;
+    //     for(i = 0; i < b; i++){
+    //         pow *= a;
+    //     }
+    //     document.write(`${a} raised to power ${b} is: <b>${pow}</b>`);
+    // }
+    // power(2, 3);
+
+
+        // Question 2
+    // function leap_year(year){
+    //     let isLeap = "";
+    //     if(year % 4 === 0){
+    //         isLeap = `${year} is a Leap Year`;
+    //     }
+    //     else{
+    //         isLeap = `${year} is <b>not</b> a Leap Year`;
+    //     }
+    //     document.write(isLeap);
+    // }
+    // let year = +prompt("Enter year");
+    // leap_year(year);
+
+
+        // Question 3
+    // function S_calc(a, b, c){
+    //     return ((a + b + c)/2);
+    // }
+    // function area(a, b, c){
+    //     let S = S_calc(a, b, c);
+    //     let area = (S * (S-a) * (S-b) * (S-c)) * 0.5;
+    //     document.write(`Area is: ${area}`);
+    // }
+    // area(2, 3, 2);
+
+
+        // Question 4
+    // function average(s1, s2, s3){
+    //     return ((s1 + s2 + s3) / 3);
+    // }
+    // function percentage(s1, s2, s3){
+    //     return (
+    //         ((s1 + s2 + s3) / 300) * 100
+    //     );
+    // }
+    // function mainFunction(s1, s2, s3){
+    //     let result = average(s1, s2, s3);
+    //     let resultPercent = percentage(s1, s2, s3);
+    //     document.write(`<p><b>Average</b> is ${result}</p>`);
+    //     document.write(`<b>Percentage</b> is ${resultPercent}%`);
+    // }
+    // mainFunction(67, 87, 98);
+
