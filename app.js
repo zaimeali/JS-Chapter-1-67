cc2b92225c1d2b844e39dc7b7dc8393087350723
zaime ali <zaimeali1997@gmail.com>
2020-07-21 21:47:11 +0500

    Ch 38-42 Done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 
         // Question 1
     // function power(a, b){
+    //     document.write("<h1>Chapters 38 - 42 | Question 1</h1>");
     //     let pow = 1;
     //     for(i = 0; i < b; i++){
     //         pow *= a;
@@ -15,6 +16,7 @@
 
         // Question 2
     // function leap_year(year){
+    //     document.write("<h1>Chapters 38 - 42 | Question 2</h1>");
     //     let isLeap = "";
     //     if(year % 4 === 0){
     //         isLeap = `${year} is a Leap Year`;
@@ -33,6 +35,7 @@
     //     return ((a + b + c)/2);
     // }
     // function area(a, b, c){
+    //     document.write("<h1>Chapters 38 - 42 | Question 3</h1>");
     //     let S = S_calc(a, b, c);
     //     let area = (S * (S-a) * (S-b) * (S-c)) * 0.5;
     //     document.write(`Area is: ${area}`);
@@ -50,6 +53,7 @@
     //     );
     // }
     // function mainFunction(s1, s2, s3){
+    //     document.write("<h1>Chapters 38 - 42 | Question 4</h1>");
     //     let result = average(s1, s2, s3);
     //     let resultPercent = percentage(s1, s2, s3);
     //     document.write(`<p><b>Average</b> is ${result}</p>`);
@@ -60,6 +64,7 @@
 
         // Question 5
     // function index_of(v, e){
+    //     document.write("<h1>Chapters 38 - 42 | Question 5</h1>");
     //     for(i = 0; i < v.length; i++){
     //         if(e === v[i]){
     //             return i;
@@ -75,6 +80,7 @@
 
         // Question 6
     // function delete_vowel(s){
+    //     document.write("<h1>Chapters 38 - 42 | Question 6</h1>");
     //     s = s.toLowerCase()
     //     let vowel = ["a", "e", "i", "o", "u"];
     //     s = s.split("");
@@ -86,27 +92,110 @@
     // }
     // delete_vowel("I am 22 year old");
 
-        // Question 7
-    function succession(s){
-        s = s.toLowerCase();
-        s = s.split("");
-        let vowel = ["a", "e", "i", "o", "u"];
-        let isJoin = false;
-        let vowelJoin = "";
 
-        for(i = 0; i < s.length; i++){
-            // if(isJoin){
-            //     vowelJoin = "";
-            //     isJoin = false;
-            // }
-            if(vowel.includes(s[i])){
-                vowelJoin += s[i];
-                isJoin = true;
-            }
-            if(vowelJoin.length > 1){
-                console.log(vowelJoin);
-                vowelJoin = "";
-            }
-        }
-    }
-    succession("Pleases read this application and give me gratuity");
+        // Question 7
+    // function succession(s){
+    //     document.write("<h1>Chapters 38 - 42 | Question 7</h1>");
+    //     s = s.toLowerCase();
+    //     s = s.split("");
+    //     let vowel = ["a", "e", "i", "o", "u"];
+    //     let isJoin = false;
+    //     let vowelJoin = "";
+    //     let j = 0;
+    //     for(i = 0; i < s.length; i++){
+    //         switch(s[i]){
+    //             case "a":
+    //                 j = i + 1;
+    //                 if(vowel.includes(s[j]) && j <= s.length){
+    //                     document.write(`<p>${s[i] + s[j]}</p>`);
+    //                 }
+    //                 break;
+    //             case "e":
+    //                 j = i + 1;
+    //                 if(vowel.includes(s[j]) && j <= s.length){
+    //                     document.write(`<p>${s[i] + s[j]}</p>`);
+    //                 }
+    //                 break;
+    //             case "i":
+    //                 j = i + 1;
+    //                 if(vowel.includes(s[j]) && j <= s.length){
+    //                     document.write(`<p>${s[i] + s[j]}</p>`);
+    //                 }
+    //                 break;
+    //             case "o":
+    //                 j = i + 1;
+    //                 if(vowel.includes(s[j]) && j <= s.length){
+    //                     document.write(`<p>${s[i] + s[j]}</p>`);
+    //                 }
+    //                 break;
+    //             case "u":
+    //                 j = i + 1;
+    //                 if(vowel.includes(s[j]) && j <= s.length){
+    //                     document.write(`<p>${s[i] + s[j]}</p>`);
+    //                 }
+    //                 break;
+    //         }
+    //     }
+    // }
+    // succession('Pleases read this application and give me gratuity');
+
+
+        // Question 8
+    // function countDecimals(value) {
+    //     if(Math.floor(value) === value) return 0;
+    //     return value.toString().split(".")[1].length || 0; 
+    // }
+    // function decimalCount(num){
+    //     if(countDecimals(num) >= 1){
+    //         return num.toFixed(2);
+    //     }
+    //     return num;
+    // }
+    // function numberWithCommas(x) {
+    //     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    // }
+    // function meters(km){
+    //     return km * 1000;
+    // }
+    // function feet(km){
+    //     return km * 3280.84;
+    // }
+    // function inches(km){
+    //     return km * 39370.1;
+    // }
+    // function centimeters(km){
+    //     return km * 100000;
+    // }
+    // function distance(city1, city2, km){
+    //     document.write("<h1>Chapters 38 - 42 | Question 8</h1>");
+    //     document.write(`<p>Distance from ${city1} to ${city2} is ${numberWithCommas(decimalCount(km))} in Kilometer</p>`);  
+    //     document.write(`<p>Distance from ${city1} to ${city2} is ${numberWithCommas(decimalCount(meters(km)))} in Meter</p>`);
+    //     document.write(`<p>Distance from ${city1} to ${city2} is ${numberWithCommas(decimalCount(feet(km)))} in Feet</p>`);
+    //     document.write(`<p>Distance from ${city1} to ${city2} is ${numberWithCommas(decimalCount(inches(km)))} in Inch</p>`);
+    //     document.write(`<p>Distance from ${city1} to ${city2} is ${numberWithCommas(decimalCount(centimeters(km)))} in Centimeter</p>`);
+    // }
+    
+    // distance("Karachi", "Lahore", 1211.9);
+
+
+        // Question 9
+    // function overtimePay(hrs, overHrWork){
+    //     document.write("<h1>Chapters 38 - 42 | Question 9</h1>");
+    //     if(hrs > 40){   
+    //         return `Overtime Pay: ${overHrWork * 12.00}`
+    //     }
+    //     return `You haven't done work more than 40 hours`;
+    // }
+    // document.write(`<p>${overtimePay(41, 2)}</p>`);
+
+
+        // Question 10
+    // function denomination(amount){
+    //     document.write("<h1>Chapters 38 - 42 | Question 10</h1>");
+    //     let hundred = parseInt(amount / 100);
+    //     let fifty = parseInt((amount % 100) / 50);
+    //     let ten = parseInt(((amount % 100) % 50) / 10);
+    //     document.write(`<p>You will have ${hundred} hundred notes ${fifty} fifty notes, ${ten} ten notes</p>`);
+    // }
+    // denomination(+prompt("Enter Amount to Withdraw"));
+    
