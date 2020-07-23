9b16c9101a459531628f7a5e9102e8901bcff5d3
zaime ali <zaimeali1997@gmail.com>
2020-07-24 01:27:11 +0500

    more than done

---
+++ ch53-58/app.js
@@ -0,0 +1,21 @@
+// 
+// Chapters 53-58 (EVENTS)
+// 
+
+
+        // Question 1
+    
+        
+
+
+        // Question 2
+    // function increaseFont(id){
+    //     let currentSize = window.getComputedStyle(document.getElementById(id)).fontSize;
+    //     let fSize = parseFloat(currentSize);
+    //     document.getElementById(id).style.fontSize = (fSize + 10) + 'px';
+    // }
+    // function decreaseFont(id){
+    //     let currentSize = window.getComputedStyle(document.getElementById(id)).fontSize;
+    //     let fSize = parseFloat(currentSize);
+    //     document.getElementById(id).style.fontSize = (fSize - 10) + 'px';
+    // }
